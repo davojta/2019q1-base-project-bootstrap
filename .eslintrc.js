9b83586baeba2e12a6a756a8f4ca1c0a52678d8b
Dzianis Sheka <dzianissheka@Dzianiss-MacBook-Pro.local>
2019-04-02 00:22:15 +0300

    feat: add eslint with airbnb config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
+};
