12bbfe4171194e4746521bea1c15325bbedcbb31
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-04-26 23:33:34 +0300

    feat: listen events from document and button

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ console.log('hello world!');
 // function init() {
 //   alert("start!");
 // }
+
+const colorPickerEl = document.getElementById('color_picker');
+console.log('colorPickerEl', colorPickerEl);
+
+// need to remove from window after dev end
+window.state = {
+    currentTool: 'colorPicker',
+};
+
+colorPickerEl.addEventListener('click', function(event) {
+    console.log('click', event);
+});
+
+document.addEventListener('click', (event) => {
+    console.log('click', event);
+    console.log('color', event.target.style.backgroundColor);
+});
