7f400db79e8f9418495180fa0d21f7d250ee7cb7
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-04-26 23:55:33 +0300

    style: fix eslint problems

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ console.log('hello world!');
 // }
 
 const colorPickerEl = document.getElementById('color_picker');
-console.log('colorPickerEl', colorPickerEl);
+// console.log('colorPickerEl', colorPickerEl);
 
 // need to remove from window after dev end
 window.state = {
@@ -14,6 +14,7 @@ window.state = {
 };
 
 colorPickerEl.addEventListener('click', (event) => {
+  // eslint-disable-next-line no-console
   console.log('click', event);
   window.state.currentTool = 'colorPicker';
   colorPickerEl.classList.add('highlight-color-picker');
@@ -22,15 +23,15 @@ colorPickerEl.addEventListener('click', (event) => {
 const headerEl = document.getElementById('header');
 
 headerEl.addEventListener('click', () => {
-  console.log('header click');
-    window.state.currentTool = 'none';
-    colorPickerEl.classList.remove('highlight-color-picker');
+  // console.log('header click');
+  window.state.currentTool = 'none';
+  colorPickerEl.classList.remove('highlight-color-picker');
 });
 
 document.addEventListener('click', (event) => {
   if (window.state.currentTool === 'colorPicker') {
-      const colorValueEl = document.getElementById('chosen_color');
-      colorValueEl.textContent = event.target.style.backgroundColor || 'none';
-    console.log('color', event.target.style.backgroundColor);
+    const colorValueEl = document.getElementById('chosen_color');
+    colorValueEl.textContent = event.target.style.backgroundColor || 'none';
+    // console.log('color', event.target.style.backgroundColor);
   }
 });
