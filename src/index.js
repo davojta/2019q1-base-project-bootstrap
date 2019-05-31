82df112f417cf8f782b90b1db3586156823355a4
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-06-01 00:16:23 +0300

    feat: add minimal markup

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ console.log('hello world!');
 // function init() {
 //   alert("start!");
 // }
+
+
+function draw() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+  
+      ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);
+    }
+  }
+
+  document.getElementById('draw').addEventListener('click', () => {
+    draw();
+  })
